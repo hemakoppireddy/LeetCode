20279f720ba2a4e52ad571acb091d1bd5e1f3a9d
hemakoppireddy <147791779+hemakoppireddy@users.noreply.github.com>
2025-12-05 15:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (99.72%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.cpp
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     bool isRobotBounded(string instructions) {
         vector<pair<int, int>> steps = {{0,1}, {1, 0}, {0, -1}, {-1, 0}};
-        int x=0, y=0;
-        int d=0;
+        int x=0, y=0, d=0;
         for(char c: instructions) {
             if(c == 'G'){
                 x += steps[d].first;
                 y += steps[d].second;
             }
-            else if(c=='L'){
-                d = (d+1)%4;
-            }
-            else if(c=='R') {
-                d = (d+3)%4;
-            }
+            else if(c=='L') d = (d+1)%4;
+            else if(c=='R') d = (d+3)%4;
         } 
-        if((x==0 && y==0) || d!=0) return true;
-        return false;
+        return (x==0 && y==0) || d!=0;
     }
 };
