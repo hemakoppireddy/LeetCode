db135c0ee375722458bbfbb1cf9ffb43b31836fc
hemakoppireddy <147791779+hemakoppireddy@users.noreply.github.com>
2025-12-13 23:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (78.25%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> freq(26, 0);
+        vector<bool> visited(26, false);
+        string stack;
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+        for (char c : s) {
+            int idx = c - 'a';
+            freq[idx]--;
+            if (visited[idx]) continue;
+            while (!stack.empty() && stack.back() > c && freq[stack.back() - 'a'] > 0) {
+                visited[stack.back() - 'a'] = false;
+                stack.pop_back();
+            }
+            stack.push_back(c);
+            visited[idx] = true;
+        }
+        return stack;
+    }
+};
