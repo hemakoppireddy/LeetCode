95a1fe488bb7f512414508c33b57eead632361fb
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-02-26 14:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.28%) - LeetSync

--- 755-reach-a-number/reach-a-number.cpp
+++ 755-reach-a-number/reach-a-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int reachNumber(int target) {
-       target = abs(target);
+        target = abs(target);
         long long sum = 0;
         int k = 0;
-        while (sum < target || (sum - target) % 2 != 0) {
+        while (sum<target || (sum-target)%2 != 0) {
             k++;
             sum += k;
         }
