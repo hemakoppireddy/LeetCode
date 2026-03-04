fed5a65d238198bffa49d4aa0c6a66571d21b961
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-03-04 15:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (10.81%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -5,7 +5,7 @@ public:
         int l = 1, r = n-2;
         while(l<=r) {
             int mid = (l+r)/2;
-            if(l==r || (arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])) return mid;
+            if(l==r ) return l;
             else if(arr[mid]<arr[mid+1]){
                 l = mid+1;
             }
