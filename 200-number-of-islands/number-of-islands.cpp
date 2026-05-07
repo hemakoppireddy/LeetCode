bee7443cc85eca6fb7d40061eb20e9bc5697b281
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-05-07 22:00:20 +0530

    Time: 26 ms (63.03%) | Memory: 16.2 MB (77.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -27,32 +27,3 @@ public:
       return count;
     }
 };
-// class Solution {
-// public:
-//     void dfs(vector<vector<char>>& grid, int row, int col) {
-//         int m = grid.size();
-//         int n = grid[0].size();
-//         if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0') {
-//             return;
-//         }
-//         grid[row][col] = '0';
-//         dfs(grid, row + 1, col);
-//         dfs(grid, row - 1, col);
-//         dfs(grid, row, col + 1);
-//         dfs(grid, row, col - 1);
-//     }
-//     int numIslands(vector<vector<char>>& grid) {
-//         int m = grid.size();
-//         int n = grid[0].size();
-//         int count = 0;
-//         for(int i = 0; i < m; i++) {
-//             for(int j = 0; j < n; j++) {
-//                 if(grid[i][j] == '1') {
-//                     count++;
-//                     dfs(grid, i, j);
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
