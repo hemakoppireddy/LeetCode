53704c91d6e23e7fb2bef1e5bd7ee5e2f3b97658
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-06-25 18:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (28.35%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -12,11 +12,13 @@ public:
             else if(nums[i]==ele) count++;
             else count--;
         }
-        count=0;
-        for(int i=0; i<n; i++) {
-            if(nums[i]==ele) count++;
-        }
-        if(count > n/2) return ele;
-        return -1;
+        // count=0;
+        // for(int i=0; i<n; i++) {
+        //     if(nums[i]==ele) count++;
+        // }
+        // if(count > n/2) return ele;
+        // return -1;
+
+        return ele;
     }
 };
