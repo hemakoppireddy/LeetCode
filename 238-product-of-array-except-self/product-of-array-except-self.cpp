8c206680fd2103f2eb3121e39aac408ab59f4dbb
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-08-06 11:01:31 +0530

    Time: 2 ms (42.91%) | Memory: 41 MB (43.15%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -7,7 +7,6 @@ public:
         int flag=0, count=0;
         if(nums[0]==0) {
             nums[0]=1;
-            // flag=1;
             count++;
         }
         for(int i=1; i<n; i++) {
@@ -16,13 +15,12 @@ public:
             }
             else if(nums[i]==0 ) {
                 product[i] = 1 * product[i-1];
-                // flag=1; 
                 count++;
             }
             else product[i] = nums[i] * product[i-1];
         }
         for(int i=0; i<n; i++) {
-            if( count==1){
+            if(count==1){
                 if(nums[i]!=0 && product[i]!=0) nums[i]=0;
                 else nums[i] = product[n-1];
             }
