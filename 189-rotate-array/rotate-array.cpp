b82283c40b4603b661117aaefe1a1416693569a4
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-08-06 15:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (46.92%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,15 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int len = nums.size();
         k = k%len;
+        reverse(nums.begin(), nums.end());
         int start = 0;
-        int end = len - 1;
-        while (start < end) {
-            std::swap(nums[start], nums[end]);
-            start++;
-            end--;
-        }
-        start = 0;
-        end = k-1;
+        int end = k-1;
         while (start < end) {
             std::swap(nums[start],nums[end]);
             start++;
