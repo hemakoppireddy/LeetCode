643f652b35574d8ba0a1b6464489d07855824642
Hema Durga Koppireddy <hemakoppireddy28@gmail.com>
2026-08-06 11:23:50 +0530

    Time: 4 ms (32.04%) | Memory: 8.4 MB (64.41%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        std::string res;
+        int max = std::max(word1.length(), word2.length());
+        for(int i=0; i<max; i++){
+            if(i<word1.length()) res+=word1[i];
+            if(i<word2.length()) res+=word2[i];
+        }
+        return res;
+    }
+};
